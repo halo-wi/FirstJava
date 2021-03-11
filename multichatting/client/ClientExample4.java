1c7337a5b1a3591bfa964ed511b3b2a735fb4b25
halo-wi <75319719+halo-wi@users.noreply.github.com>
2021-03-11 17:35:38 +0900

    Java Chatting Code
    first chatting excersise

---
+++ multichatting/client/ClientExample4.java
@@ -0,0 +1,22 @@
+package com.kosta.day16.multichatting.client;
+
+import java.net.*;
+class ClientExample4 {
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println(
+                "Usage: java ClientExample4 <user-name>");
+            return;
+        }
+        try {
+            Socket socket = new Socket("192.168.0.172", 9002);
+            Thread thread1 = new SenderThread(socket, args[0]);
+            Thread thread2 = new ReceiverThread(socket);
+            thread1.start();
+            thread2.start();
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
